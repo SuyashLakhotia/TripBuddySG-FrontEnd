7af0bd0566f38bca27db60e72aa32f7d16056661
Suyash Lakhotia <suyashlakhotia@gmail.com>
2015-12-15 23:54:14 +0800

    v1.0

---
+++ js/main.js
@@ -0,0 +1,47 @@
+// jQuery to Collapse Navbar on Scroll:
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 140) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+
+// jQuery to Add More Textboxes for Includes/Excludes
+$(function(){
+    $('#addIncludes').click(function(){
+        var li = $('.includes-arr li:first').clone().appendTo($('.includes-arr'));
+        
+        // empty the value if something is already filled in the cloned copy
+        li.children('input').val('');
+        
+        // disable button if its the 10th that was added
+        if ($('.includes-arr').children().length == 10){
+            $(this).attr('disabled', true);
+        }
+    }); 
+});
+
+$(function(){
+    $('#addExcludes').click(function(){
+        var li = $('.excludes-arr li:first').clone().appendTo($('.excludes-arr'));
+        
+        // empty the value if something is already filled in the cloned copy
+        li.children('input').val('');
+        
+        // disable button if its the 10th that was added
+        if ($('.excludes-arr').children().length == 10){
+            $(this).attr('disabled', true);
+        }
+    }); 
+});
+
+
+// JavaScript for scrollTo Animations:
+$(document).ready(function() {
+  $('a[href^="#"]').click(function(e) {
+    e.preventDefault();
+    $(window).stop(true).scrollTo(this.hash, {duration:1000, interrupt:false}, {'axis':'y'});
+  });
+});
---
+++ js/tabbed-legal.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function () {
+    jQuery('.legal-pills .nav-pills a').on('click', function (e) {
+        var currentAttrValue = jQuery(this).attr('href');
+
+        // Show & Hide Tabs:
+        jQuery('.legal-content ' + currentAttrValue).show().siblings().hide();
+
+        // Change & Remove current tab to active:
+        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+
+        e.preventDefault();
+    });
+});
