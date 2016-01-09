a0cc4d872afee08d6d23d40853cf960205114ea6
Suyash Lakhotia <suyashlakhotia@gmail.com>
2016-01-10 02:14:04 +0800

    v1.21 - Replaced scrollTo() w/ Custom JS, Minor Design Tweaks & Updated Contact E-Mail

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ TripBuddy.sg (c) 2015. All rights reserved.
 *************************************************************************/
 
 // jQuery to Collapse Navbar on Scroll:
-$(window).scroll(function() {
+$(window).scroll(function () {
     if ($(".navbar").offset().top > 70) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
@@ -13,39 +13,47 @@ $(window).scroll(function() {
 
 
 // jQuery to Add More Textboxes for Includes/Excludes
-$(function(){
-    $('#addIncludes').click(function(){
+$(function () {
+    $('#addIncludes').click(function () {
         var li = $('.includes-arr li:first').clone().appendTo($('.includes-arr'));
-        
+
         // empty the value if something is already filled in the cloned copy
         li.children('input').val('');
-        
+
         // disable button if its the 10th that was added
-        if ($('.includes-arr').children().length == 10){
+        if ($('.includes-arr').children().length == 10) {
             $(this).attr('disabled', true);
         }
-    }); 
+    });
 });
 
-$(function(){
-    $('#addExcludes').click(function(){
+$(function () {
+    $('#addExcludes').click(function () {
         var li = $('.excludes-arr li:first').clone().appendTo($('.excludes-arr'));
-        
+
         // empty the value if something is already filled in the cloned copy
         li.children('input').val('');
-        
+
         // disable button if its the 10th that was added
-        if ($('.excludes-arr').children().length == 10){
+        if ($('.excludes-arr').children().length == 10) {
             $(this).attr('disabled', true);
         }
-    }); 
+    });
 });
 
 
-// JavaScript for scrollTo Animations:
-$(document).ready(function() {
-  $('a[href^="#"]').click(function(e) {
-    e.preventDefault();
-    $(window).stop(true).scrollTo(this.hash, {duration:1000, interrupt:false}, {'axis':'y'});
-  });
+// JavaScript for Anchor Link Scroll Animations:
+$(function () {
+    $('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
 });
